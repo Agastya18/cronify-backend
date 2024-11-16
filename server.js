4aff0d36b3c0f4290776ded40b319a8204c08c53
agastya gaur <agastyagaur17@gmail.com>
2024-11-16 23:58:21 +0530

    project backend setup

---
+++ server.js
@@ -0,0 +1,20 @@
+import "dotenv/config"
+
+import express from "express"
+import cors from "cors"
+
+const PORT= process.env.PORT || 4000
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+}   )
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`)
+  })
